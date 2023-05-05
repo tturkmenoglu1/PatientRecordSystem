4cd783db4c3e856236af596078d61d2e176d6ed0
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-05-05 23:36:04 +0400

    05/05

--- src/main/java/com/patientrecord/domain/User.java
+++ src/main/java/com/patientrecord/domain/User.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "t_user")
