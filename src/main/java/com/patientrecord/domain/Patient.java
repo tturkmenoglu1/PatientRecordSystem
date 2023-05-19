90d002815f9ceff5fe146e9ea8a0b8031800be29
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-05-19 23:36:29 +0400

    05/19 v2

--- src/main/java/com/patientrecord/domain/Patient.java
+++ src/main/java/com/patientrecord/domain/Patient.java
@@ -1,11 +1,14 @@
 package com.patientrecord.domain;
 
+import com.patientrecord.domain.enums.Gender;
+import com.patientrecord.domain.enums.GroupName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -22,45 +25,46 @@ public class Patient {
 
     @Enumerated(EnumType.STRING)
     @Column
-    private String groupName;
+    private GroupName groupName;
 
     @Column(length = 30, nullable = false)
     private String firstName;
 
-    @Column(length = 30, nullable = false)
+    @Column(length = 30)
     private String lastName;
 
-    @Column(length = 30, nullable = false)
-    private String birthPlace;
+    @Column
+    private LocalDateTime birthPlace;
 
-    @Column(length = 30, nullable = false)
+    @Column(length = 30)
     private String birthDate;
 
-    @Column(length = 30, nullable = false)
-    private String gender;
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Gender gender;
 
-    @Column(length = 100, nullable = false)
+    @Column(length = 100, unique = true)
     private String email;
 
     @Column(length = 15, nullable = false)
     private String phoneNumber;
 
-    @Column(length = 100, nullable = false)
+    @Column(length = 100)
     private String address;
 
-    @Column(length = 500, nullable = false)
+    @Column(length = 500)
     private String complain;
 
-    @Column(length = 3500, nullable = false)
+    @Column(length = 3500)
     private String story;
 
     @Column(length = 100, nullable = false)
     private String treat;
 
-    @Column(length = 300, nullable = false)
+    @Column(length = 300)
     private String medicine;
 
-    @Column(length = 500, nullable = false)
+    @Column(length = 500)
     private String advice;
 
 }
---
+++ src/main/java/com/patientrecord/domain/enums/Gender.java
@@ -0,0 +1,10 @@
+package com.patientrecord.domain.enums;
+
+public enum Gender {
+
+    ERKEK,
+
+    KADIN,
+
+    BILINMIYOR;
+}
