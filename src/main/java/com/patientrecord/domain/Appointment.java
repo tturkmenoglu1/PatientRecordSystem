e1df5fde45e4640649bc34453c2d07e2d9b53332
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-05-20 23:49:13 +0400

    05/20

--- src/main/java/com/patientrecord/domain/Appointment.java
+++ src/main/java/com/patientrecord/domain/Appointment.java
@@ -11,6 +11,7 @@ public class Appointment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String patientName;
 
     private LocalDateTime appointmentDate;
