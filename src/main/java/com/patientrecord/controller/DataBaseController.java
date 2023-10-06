f4cbda2884fbd8f37a11863ba81e008f079d037c
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2023-10-06 23:52:11 +0400

    10/06

---
+++ src/main/java/com/patientrecord/controller/DataBaseController.java
@@ -0,0 +1,23 @@
+package com.patientrecord.controller;
+
+import com.patientrecord.dto.response.PRResponse;
+import com.patientrecord.service.DataBaseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/database")
+@RequiredArgsConstructor
+public class DataBaseController {
+
+    public final DataBaseService dataBaseService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<PRResponse>
+
+}
---
+++ src/main/java/com/patientrecord/service/DataBaseService.java
@@ -0,0 +1,8 @@
+package com.patientrecord.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataBaseService {
+
+}
